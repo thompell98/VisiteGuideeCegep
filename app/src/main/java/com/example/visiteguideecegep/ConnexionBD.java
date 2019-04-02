32ddeeb5bcf303efb807a405f3aa801b932af5d9
B438P-21\admin <pellthomas@hotmail.com>
2019-04-02 17:10:11 -0400

    Possibilité de trouver un Local dans la BD à l`aide de son numéro

--- app/src/main/java/com/example/visiteguideecegep/ConnexionBD.java
+++ app/src/main/java/com/example/visiteguideecegep/ConnexionBD.java
@@ -3,23 +3,31 @@ package com.example.visiteguideecegep;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class ConnexionBD extends AppCompatActivity
 {
+    private static final String TAG = "ConnexionBD";
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     TextView textViewNumero = null;
     TextView textViewNom = null;
     TextView textViewEtage = null;
+    EditText editTextSearch = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -36,17 +44,19 @@ public class ConnexionBD extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                getLocalData();
+                editTextSearch = (EditText) findViewById(R.id.editTextSearch);
+                String numeroLocal = editTextSearch.getText().toString();
+                getLocalData(numeroLocal);
             }
         });
     }
 
-    private void getLocalData()
+    private void getDataFromDocumentPath(String documentPath)
     {
         textViewNumero = (TextView) findViewById(R.id.textViewNumero);
         textViewNom = (TextView) findViewById(R.id.textViewNom);
         textViewEtage = (TextView) findViewById(R.id.textViewEtage);
-        DocumentReference user = db.collection("Locaux").document("k8qwfJoomwxO4wcilSa3");
+        DocumentReference user = db.collection("Locaux").document(documentPath);
         user.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>()
         {
             @Override
@@ -68,5 +78,38 @@ public class ConnexionBD extends AppCompatActivity
                 Toast.makeText(getApplicationContext(), "This is my Toast message!", Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    private void getLocalData(String numeroLocal)
+    {
+        textViewNumero = (TextView) findViewById(R.id.textViewNumero);
+        textViewNom = (TextView) findViewById(R.id.textViewNom);
+        textViewEtage = (TextView) findViewById(R.id.textViewEtage);
+        db.collection("Locaux")
+                .whereEqualTo("numero", numeroLocal)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task)
+                    {
+                        if (task.isSuccessful())
+                        {
+                            for (QueryDocumentSnapshot document : task.getResult())
+                            {
+
+                                    textViewNumero.setText(document.get("numero").toString());
+                                    textViewNom.setText(document.get("nom").toString());
+                                    textViewEtage.setText(document.get("Etage").toString());
+                            }
+                        }
+                        else
+                            {
+                                Toast.makeText(getApplicationContext(), task.getException().toString(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                });
+
     }
 }
