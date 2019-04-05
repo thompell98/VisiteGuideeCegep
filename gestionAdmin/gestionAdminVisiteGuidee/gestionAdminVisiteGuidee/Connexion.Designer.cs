ca2fd3c04ff83b2fe0559810368b35e8d2584a6a
B438P-21\admin <pellthomas@hotmail.com>
2019-04-05 16:16:29 -0400

    Connexion Firestore en c#

---
+++ gestionAdmin/gestionAdminVisiteGuidee/gestionAdminVisiteGuidee/Connexion.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace gestionAdminVisiteGuidee
+{
+    partial class Connexion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSeConnecter = new System.Windows.Forms.Button();
+            this.textBoxNomUtilisateur = new System.Windows.Forms.TextBox();
+            this.textBoxMotDePasse = new System.Windows.Forms.TextBox();
+            this.labelNomUtilisateur = new System.Windows.Forms.Label();
+            this.labelMotDePasse = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSeConnecter
+            // 
+            this.buttonSeConnecter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSeConnecter.Location = new System.Drawing.Point(201, 166);
+            this.buttonSeConnecter.Name = "buttonSeConnecter";
+            this.buttonSeConnecter.Size = new System.Drawing.Size(128, 35);
+            this.buttonSeConnecter.TabIndex = 0;
+            this.buttonSeConnecter.Text = "Se connecter";
+            this.buttonSeConnecter.UseVisualStyleBackColor = true;
+            this.buttonSeConnecter.Click += new System.EventHandler(this.buttonSeConnecter_Click);
+            // 
+            // textBoxNomUtilisateur
+            // 
+            this.textBoxNomUtilisateur.Location = new System.Drawing.Point(265, 40);
+            this.textBoxNomUtilisateur.Name = "textBoxNomUtilisateur";
+            this.textBoxNomUtilisateur.Size = new System.Drawing.Size(128, 20);
+            this.textBoxNomUtilisateur.TabIndex = 1;
+            // 
+            // textBoxMotDePasse
+            // 
+            this.textBoxMotDePasse.Location = new System.Drawing.Point(265, 97);
+            this.textBoxMotDePasse.Name = "textBoxMotDePasse";
+            this.textBoxMotDePasse.Size = new System.Drawing.Size(128, 20);
+            this.textBoxMotDePasse.TabIndex = 2;
+            // 
+            // labelNomUtilisateur
+            // 
+            this.labelNomUtilisateur.AutoSize = true;
+            this.labelNomUtilisateur.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNomUtilisateur.Location = new System.Drawing.Point(111, 40);
+            this.labelNomUtilisateur.Name = "labelNomUtilisateur";
+            this.labelNomUtilisateur.Size = new System.Drawing.Size(148, 20);
+            this.labelNomUtilisateur.TabIndex = 3;
+            this.labelNomUtilisateur.Text = "Nom d\'utilisateur:";
+            // 
+            // labelMotDePasse
+            // 
+            this.labelMotDePasse.AutoSize = true;
+            this.labelMotDePasse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMotDePasse.Location = new System.Drawing.Point(137, 97);
+            this.labelMotDePasse.Name = "labelMotDePasse";
+            this.labelMotDePasse.Size = new System.Drawing.Size(122, 20);
+            this.labelMotDePasse.TabIndex = 4;
+            this.labelMotDePasse.Text = "Mot de passe:";
+            // 
+            // Connexion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 241);
+            this.Controls.Add(this.labelMotDePasse);
+            this.Controls.Add(this.labelNomUtilisateur);
+            this.Controls.Add(this.textBoxMotDePasse);
+            this.Controls.Add(this.textBoxNomUtilisateur);
+            this.Controls.Add(this.buttonSeConnecter);
+            this.Name = "Connexion";
+            this.Text = "Connexion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSeConnecter;
+        private System.Windows.Forms.TextBox textBoxNomUtilisateur;
+        private System.Windows.Forms.TextBox textBoxMotDePasse;
+        private System.Windows.Forms.Label labelNomUtilisateur;
+        private System.Windows.Forms.Label labelMotDePasse;
+    }
+}
---
+++ gestionAdmin/gestionAdminVisiteGuidee/gestionAdminVisiteGuidee/Connexion.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Cloud.Firestore;
+
+namespace gestionAdminVisiteGuidee
+{
+    public partial class Connexion : Form
+    {
+        FirestoreDb db = FirestoreDb.Create("visiteguideecegep-f394b");   
+
+        public Connexion()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSeConnecter_Click(object sender, EventArgs e)
+        {
+            String nomUtilisateur = textBoxNomUtilisateur.Text;
+            String motDePasse = textBoxMotDePasse.Text;
+            SeConnecter(nomUtilisateur, motDePasse);
+        }
+
+        private async void SeConnecter(String nomUtilisateur, String motDePasse)
+        {
+            if (nomUtilisateur == "" || motDePasse == "")
+            {
+                MessageBox.Show("Veuillez remplir chacun des champs pour vous connecter.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                CollectionReference usersRef = db.Collection("Admins");
+                try
+                {
+                    QuerySnapshot snapshot = await usersRef.GetSnapshotAsync();
+                    foreach (DocumentSnapshot document in snapshot.Documents)
+                    {
+                        Dictionary<string, object> documentDictionary = document.ToDictionary();
+                        if (documentDictionary["Username"].ToString() == nomUtilisateur
+                            && documentDictionary["Password"].ToString() == motDePasse)
+                        {
+                            MessageBox.Show("Vous êtes connecté.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Mauvais nom d'utilisateur ou mot de passe, veuillez réessayer.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Impossible de se connecter à la base de données. Veuillez vérifier votre connexion internet.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+    }
+}
