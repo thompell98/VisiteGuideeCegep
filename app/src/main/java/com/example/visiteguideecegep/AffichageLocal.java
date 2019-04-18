6db202046fc0cacf79d2e5a34150479eff017936
B438P-21\admin <pellthomas@hotmail.com>
2019-04-18 11:37:20 -0400

    Affichage image de Cloud Firestore

--- app/src/main/java/com/example/visiteguideecegep/AffichageLocal.java
+++ app/src/main/java/com/example/visiteguideecegep/AffichageLocal.java
@@ -1,46 +1,84 @@
 package com.example.visiteguideecegep;
 
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-public class AffichageLocal extends AppCompatActivity {
+import java.time.Instant;
 
-    FirebaseStorage storageRef = null;
-    StorageReference cafeteriaRef = null;
+public class AffichageLocal extends AppCompatActivity {
+    FirebaseStorage storage = null;
+    StorageReference storageRef = null;
+    StorageReference imageRef = null;
+    FirebaseAuth mAuth = null;
+    ImageView imageView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_affichage_local);
+        imageView = (ImageView) findViewById(R.id.imageView);
         setCloudStorage();
-        downloadImage();
     }
 
     private void setCloudStorage()
     {
-        storageRef = FirebaseStorage.getInstance("gs://visiteguideecegep-f394b.appspot.com");
-        cafeteriaRef = storageRef.getReference("A-111/cafeteria.jpg");
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
+        imageRef = storageRef.child("A-111/cafeteria.jpg");
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        mAuth.signInAnonymously();
+        if (user != null) {
+            downloadImage();
+        } else {
+            signInAnonymously();
+        }
+    }
+
+    private void signInAnonymously() {
+        mAuth.signInAnonymously().addOnSuccessListener(this, new  OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                downloadImage();
+            }
+        })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Toast.makeText(getApplicationContext(), "Auth failure", Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 
     private void downloadImage()
     {
-        final long ONE_MEGABYTE = 1024 * 1024;
-        cafeteriaRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+        imageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
             @Override
-            public void onSuccess(byte[] bytes) {
-                // Data for "images/island.jpg" is returns, use this as needed
+            public void onSuccess(Uri uri) {
+                Glide.with(getApplicationContext())
+                        .load(uri)
+                        .into(imageView);
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
-            public void onFailure(@NonNull Exception exception) {
-                // Handle any errors
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Download image error", Toast.LENGTH_LONG).show();
             }
         });
     }
--- app/src/main/java/com/example/visiteguideecegep/MainActivity.java
+++ app/src/main/java/com/example/visiteguideecegep/MainActivity.java
@@ -6,12 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,16 +26,29 @@ public class MainActivity extends AppCompatActivity {
                 redirectionCamera();
             }
         });
-
+        findViewById(R.id.buttonAffichageLocal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectionAffichageLocal();
+            }
+        });
     }
-private void redirectionCamera()
-{
-    Intent sendUserToSignUpOrLogin=new Intent(this, CameraActivity.class);
-    startActivity(sendUserToSignUpOrLogin);
-}
+
+    private void redirectionCamera()
+    {
+        Intent sendUserToSignUpOrLogin=new Intent(this, CameraActivity.class);
+        startActivity(sendUserToSignUpOrLogin);
+    }
+
     private void redirectionDb()
     {
         Intent sendUserToSignUpOrLogin=new Intent(this, ConnexionBD.class);
         startActivity(sendUserToSignUpOrLogin);
+    }
+
+    private void redirectionAffichageLocal()
+    {
+        Intent sendUserToAffichageLocal = new Intent(this, AffichageLocal.class);
+        startActivity(sendUserToAffichageLocal);
     }
 }
