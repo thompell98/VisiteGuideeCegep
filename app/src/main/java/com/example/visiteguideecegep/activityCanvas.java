7496c0745d8a6af9c1e9a60c1db96ab3dfbfbd27
B438P-21\admin <pellthomas@hotmail.com>
2019-05-01 16:12:14 -0400

    developpement de l'algorithme pour trouver le chemin le plus court entre deux points

--- app/src/main/java/com/example/visiteguideecegep/activityCanvas.java
+++ app/src/main/java/com/example/visiteguideecegep/activityCanvas.java
@@ -14,31 +14,31 @@ import com.google.firestore.v1.StructuredQuery;
 
 import java.util.ArrayList;
 
-public class activityCanvas extends AppCompatActivity
-{
+public class activityCanvas extends AppCompatActivity {
     Canvas canvas;
     ImageView mImageView;
     Bitmap bitmap;
     Intersection lesIntersections[] = new Intersection[5];
+
+    ArrayList<Intersection> lesIntersectionsARelier = new ArrayList<Intersection>();
     Intersection intersectionATrouver;
+    Intersection intersectionDepart;
+    boolean connecte;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_canvas);
         mImageView = (ImageView) findViewById(R.id.imageView);
         setCanvas();
         ajouterLesIntersections();
-        dessinerIntersection(lesIntersections[0]);
-        dessinerIntersection(lesIntersections[1]);
-        dessinerIntersection(lesIntersections[2]);
+        intersectionDepart = lesIntersections[4];
         intersectionATrouver = lesIntersections[2];
-        trouverCheminLePlusCourt(lesIntersections[0]);
+        connecte = false;
+        djikatra();
     }
 
-    private void setCanvas()
-    {
+    private void setCanvas() {
         bitmap = Bitmap.createBitmap(
                 500,
                 300,
@@ -49,8 +49,7 @@ public class activityCanvas extends AppCompatActivity
         mImageView.setImageBitmap(bitmap);
     }
 
-    private void dessinerLigne(Intersection intersection1, Intersection intersection2)
-    {
+    private void dessinerLigne(Intersection intersection1, Intersection intersection2) {
         Paint paint = new Paint();
         paint.setColor(Color.RED);
         paint.setStyle(Paint.Style.STROKE);
@@ -60,34 +59,68 @@ public class activityCanvas extends AppCompatActivity
         mImageView.setImageBitmap(bitmap);
     }
 
-    private void ajouterLesIntersections()
-    {
-       lesIntersections[0] = new Intersection(0, 30, 250,  new int[]{1, 2});
-       lesIntersections[1] = new Intersection(1, 60, 30,  new int[]{0, 2});
-       lesIntersections[2] = new Intersection(2, 100, 80,  new int[]{0, 1});
+    private void afficherTrajet() {
+        for (int cpt = 0; cpt < lesIntersectionsARelier.size() - 1; cpt++) {
+            dessinerLigne(lesIntersectionsARelier.get(cpt), lesIntersectionsARelier.get(cpt + 1));
+        }
     }
 
-    private void dessinerIntersection(Intersection uneIntersection)
-    {
+    private void ajouterLesIntersections() {
+        lesIntersections[0] = new Intersection(0, 30, 250, new int[]{1, 2});
+        lesIntersections[1] = new Intersection(1, 60, 30, new int[]{0, 3});
+        lesIntersections[2] = new Intersection(2, 100, 80, new int[]{0, 3, 4});
+        lesIntersections[3] = new Intersection(3, 100, 50, new int[]{1, 2, 4});
+        lesIntersections[4] = new Intersection(4, 95, 20, new int[]{1, 3});
+        dessinerIntersection(lesIntersections[0]);
+        dessinerIntersection(lesIntersections[1]);
+        dessinerIntersection(lesIntersections[2]);
+        dessinerIntersection(lesIntersections[3]);
+        dessinerIntersection(lesIntersections[4]);
+    }
+
+    private void dessinerIntersection(Intersection uneIntersection) {
         Paint paint = new Paint();
         canvas.drawCircle((float) uneIntersection.x, (float) uneIntersection.y, 10, paint);
     }
 
-    private double calculerDistanceEntreDeuxPoints(Intersection intersection1, Intersection intersection2)
-    {
+    private double calculerDistanceEntreDeuxPoints(Intersection intersection1, Intersection intersection2) {
         return Math.sqrt(Math.pow(intersection2.x - intersection1.x, 2) + Math.pow(intersection2.y - intersection1.y, 2));
     }
 
-    private void trouverCheminLePlusCourt(Intersection intersectionInitial)
-    {
-        double[] lesLongueursPossibles;
-
-        for(int cpt = 0; cpt < intersectionInitial.numerosIntersectionsReliees.size(); cpt++)
-        {
-            //if (lesIntersections[intersectionInitial.numerosIntersectionsReliees.get(cpt)] != )
+    private void djikatra() {
+        while (connecte == false) {
+            if (lesIntersectionsARelier.size() == 0) {
+                lesIntersectionsARelier.add(intersectionDepart);
+                ajouterProchaineIntersectionAuTrajet(intersectionDepart);
+            } else {
+                ajouterProchaineIntersectionAuTrajet(lesIntersectionsARelier.get(lesIntersectionsARelier.size() - 1));
+            }
         }
-        intersectionInitial.utilise = true;
-
+        afficherTrajet();
     }
 
+    private void ajouterProchaineIntersectionAuTrajet(Intersection intersectionInitial) {
+        for (int cpt = 0; cpt < intersectionInitial.numerosIntersectionsReliees.size(); cpt++) {
+            if (intersectionInitial.numerosIntersectionsReliees.get(cpt) == intersectionATrouver.numero) {
+                lesIntersectionsARelier.add(lesIntersections[intersectionInitial.numerosIntersectionsReliees.get(cpt)]);
+                connecte = true;
+            }
+        }
+
+        if (connecte != true) {
+            Intersection intersectionLaPlusProche = null;
+            for (int cpt = 0; cpt < intersectionInitial.numerosIntersectionsReliees.size(); cpt++) {
+                if (cpt == 0) {
+                    intersectionLaPlusProche = lesIntersections[intersectionInitial.numerosIntersectionsReliees.get(cpt)];
+                } else if (lesIntersections[intersectionInitial.numerosIntersectionsReliees.get(cpt)].utilise == false) {
+                    if (calculerDistanceEntreDeuxPoints(intersectionInitial, lesIntersections[intersectionInitial.numerosIntersectionsReliees.get(cpt)]) <
+                            calculerDistanceEntreDeuxPoints(intersectionInitial, lesIntersections[intersectionInitial.numerosIntersectionsReliees.get(cpt - 1)])) {
+                        intersectionLaPlusProche = lesIntersections[intersectionInitial.numerosIntersectionsReliees.get(cpt)];
+                    }
+                }
+            }
+            intersectionInitial.utilise = true;
+            lesIntersectionsARelier.add(intersectionLaPlusProche);
+        }
+    }
 }
