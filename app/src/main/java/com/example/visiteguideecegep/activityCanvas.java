3e92c6cba46176eb0e5a25008821819a5c5db3a1
B438P-21\admin <pellthomas@hotmail.com>
2019-05-02 11:33:39 -0400

    Correction de l'intersection depart et finale

--- app/src/main/java/com/example/visiteguideecegep/activityCanvas.java
+++ app/src/main/java/com/example/visiteguideecegep/activityCanvas.java
@@ -18,9 +18,8 @@ public class activityCanvas extends AppCompatActivity {
     Canvas canvas;
     ImageView mImageView;
     Bitmap bitmap;
-    Intersection lesIntersections[] = new Intersection[5];
-
-    ArrayList<Intersection> lesIntersectionsARelier = new ArrayList<Intersection>();
+    Intersection lesIntersections[] = new Intersection[7];
+    ArrayList<Intersection> lesIntersectionsARelier = new ArrayList<>();
     Intersection intersectionATrouver;
     Intersection intersectionDepart;
     boolean connecte;
@@ -29,13 +28,13 @@ public class activityCanvas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_canvas);
-        mImageView = (ImageView) findViewById(R.id.imageView);
+        mImageView = findViewById(R.id.imageView);
         setCanvas();
         ajouterLesIntersections();
-        intersectionDepart = lesIntersections[4];
-        intersectionATrouver = lesIntersections[2];
+        intersectionDepart = lesIntersections[6];
+        intersectionATrouver = lesIntersections[1];
         connecte = false;
-        djikatra();
+        djikastra();
     }
 
     private void setCanvas() {
@@ -66,16 +65,16 @@ public class activityCanvas extends AppCompatActivity {
     }
 
     private void ajouterLesIntersections() {
-        lesIntersections[0] = new Intersection(0, 30, 250, new int[]{1, 2});
-        lesIntersections[1] = new Intersection(1, 60, 30, new int[]{0, 3});
-        lesIntersections[2] = new Intersection(2, 100, 80, new int[]{0, 3, 4});
-        lesIntersections[3] = new Intersection(3, 100, 50, new int[]{1, 2, 4});
-        lesIntersections[4] = new Intersection(4, 95, 20, new int[]{1, 3});
-        dessinerIntersection(lesIntersections[0]);
-        dessinerIntersection(lesIntersections[1]);
-        dessinerIntersection(lesIntersections[2]);
-        dessinerIntersection(lesIntersections[3]);
-        dessinerIntersection(lesIntersections[4]);
+        lesIntersections[0] = new Intersection(0, 250, 20, new int[]{1});
+        lesIntersections[1] = new Intersection(1, 250, 70, new int[]{0, 2});
+        lesIntersections[2] = new Intersection(2, 250, 120, new int[]{1, 3});
+        lesIntersections[3] = new Intersection(3, 250, 170, new int[]{2, 4});
+        lesIntersections[4] = new Intersection(4, 250, 220, new int[]{3, 5, 6});
+        lesIntersections[5] = new Intersection(5, 200, 220, new int[]{4});
+        lesIntersections[6] = new Intersection(6, 300, 220, new int[]{4});
+        for (int cpt = 0; cpt < lesIntersections.length; cpt++) {
+            dessinerIntersection(lesIntersections[cpt]);
+        }
     }
 
     private void dessinerIntersection(Intersection uneIntersection) {
@@ -87,7 +86,12 @@ public class activityCanvas extends AppCompatActivity {
         return Math.sqrt(Math.pow(intersection2.x - intersection1.x, 2) + Math.pow(intersection2.y - intersection1.y, 2));
     }
 
-    private void djikatra() {
+    private void djikastra() {
+        if (intersectionDepart.numero < intersectionATrouver.numero) {
+            Intersection intersectionTemp = intersectionDepart;
+            intersectionDepart = intersectionATrouver;
+            intersectionATrouver = intersectionTemp;
+        }
         while (connecte == false) {
             if (lesIntersectionsARelier.size() == 0) {
                 lesIntersectionsARelier.add(intersectionDepart);
@@ -102,11 +106,11 @@ public class activityCanvas extends AppCompatActivity {
     private void ajouterProchaineIntersectionAuTrajet(Intersection intersectionInitial) {
         for (int cpt = 0; cpt < intersectionInitial.numerosIntersectionsReliees.size(); cpt++) {
             if (intersectionInitial.numerosIntersectionsReliees.get(cpt) == intersectionATrouver.numero) {
+                intersectionInitial.utilise = true;
                 lesIntersectionsARelier.add(lesIntersections[intersectionInitial.numerosIntersectionsReliees.get(cpt)]);
                 connecte = true;
             }
         }
-
         if (connecte != true) {
             Intersection intersectionLaPlusProche = null;
             for (int cpt = 0; cpt < intersectionInitial.numerosIntersectionsReliees.size(); cpt++) {
