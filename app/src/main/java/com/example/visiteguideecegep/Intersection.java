25ff6c6b091836c1cdf30c7f9125d7a74a035dcf
Thomas <pellthomas@hotmail.com>
2019-05-16 22:18:24 -0400

    Supression attribut EscalierLePlusProche  dans la classe Intersection

--- app/src/main/java/com/example/visiteguideecegep/Intersection.java
+++ app/src/main/java/com/example/visiteguideecegep/Intersection.java
@@ -5,13 +5,11 @@ import java.util.ArrayList;
 public class Intersection {
     Coordonnee coordonnee;
     boolean utilise;
-    int numeroEscalier;
     ArrayList<Integer> numerosIntersectionsReliees = new ArrayList<>();
 
-    public Intersection( float x, float y, int numeroEscalier, int[] numerosIntersectionsReliees) {
+    public Intersection( float x, float y, int[] numerosIntersectionsReliees) {
         this.coordonnee = new Coordonnee(x, y);
         this.utilise = false;
-        this.numeroEscalier = numeroEscalier;
         for (int cpt = 0; cpt < numerosIntersectionsReliees.length; cpt++)
         {
             this.numerosIntersectionsReliees.add(numerosIntersectionsReliees[cpt]);
